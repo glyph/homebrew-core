17fe3b497c342a9f00dd1c9755538f15cf3e3582
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-22 11:05:56 +0000

    node@20 20.19.1

--- Formula/n/node@20.rb
+++ Formula/n/node@20.rb
@@ -1,10 +1,9 @@
 class NodeAT20 < Formula
   desc "Platform built on V8 to build network applications"
   homepage "https://nodejs.org/"
-  url "https://nodejs.org/dist/v20.19.0/node-v20.19.0.tar.xz"
-  sha256 "5ac2516fc905b6a0bc1a33e7302937eac664a820b887cc86bd48c035fba392d7"
+  url "https://nodejs.org/dist/v20.19.1/node-v20.19.1.tar.xz"
+  sha256 "5587b23e907d0c7af2ea8a8deb33ec50010453b46dbb3df5987c5678eee5ed51"
   license "MIT"
-  revision 1
 
   livecheck do
     url "https://nodejs.org/dist/"
